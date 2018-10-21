5305f6f7d22cf83c709749af6eac8b2d0d288206
Thavi <thavit97@gmail.com>
2018-10-21 14:15:42 +0100

    Refactoring

--- Lab2/src/RouteSearch.java
+++ Lab2/src/RouteSearch.java
@@ -7,7 +7,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
-public class Main {
+public class RouteSearch {
 
 //	private static int[][] graph;
 //	private char[] route;
@@ -21,8 +21,7 @@ public class Main {
 	private ArrayList<Integer> bestRouteLocalSearch;
 	private ArrayList<Integer> bestRouteRandomSearch;
 
-	public Main() {
-//		createGraph();
+	public RouteSearch(int setTime) {
 		try {
 			loadFile(16);
 		} catch (FileNotFoundException e) {
@@ -30,16 +29,20 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		double localSearchCSVCost = localSearchCSV(2700000);
-		System.out.println("Local Search: The best route is " + bestRouteLocalSearch + " and it costs " + localSearchCSVCost);
+		System.out.println("Calculating the best route using Local Search. This will take " + setTime/1000 + " seconds...");
+		double localSearchCSVCost = localSearchCSV(setTime);		
+		System.out.println("Calculating the best route using Random Search. This will take " + setTime/1000 + " seconds...");
+		double randomSearchCSVCost = randomSearchCSV(setTime);
 		
-		double randomSearchCSVCost = randomSearchCSV(2700000);
+		System.out.println("---------------------------------------------");
+		
+		System.out.println("Local Search: The best route is " + bestRouteLocalSearch + " and it costs " + localSearchCSVCost);
 		System.out.println("Random Search: The best route is " + bestRouteRandomSearch + " and it costs " + randomSearchCSVCost);
 		//System.out.println(timedRouteCSV(2000));
 	}
 
 	public static void main(String[] args) {	
-		new Main();
+		new RouteSearch(10000);
 	}
 	
 	static int factorial(int n){
@@ -50,185 +53,7 @@ public class Main {
 		return res;
 	}
 
-//	private int[][] createGraph() {
-//		graph = new int[4][4];
-//		// A = 0
-//		graph[A][A] = 0; // AA
-//		graph[A][B] = 20; // AB
-//		graph[A][C] = 41; // AC
-//		graph[A][D] = 35; // AD
-//
-//		// B = 1
-//		graph[B][A] = 20; // BA
-//		graph[B][B] = 0; // BB
-//		graph[B][C] = 30; // BC
-//		graph[B][D] = 34; // BD
-//
-//		// C = 2
-//		graph[C][A] = 42; // CA
-//		graph[C][B] = 30; // CB
-//		graph[C][C] = 0; // CC
-//		graph[C][D] = 12; // CD
-//
-//		// D = 3
-//		graph[D][A] = 35; // DA
-//		graph[D][B] = 34; // DB
-//		graph[D][C] = 12; // DC
-//		graph[D][D] = 0; // DD
-//
-//		return graph;
-//	}
-//
-//	/**
-//	 * Create a route with x number of cities
-//	 * 
-//	 * @param numCities
-//	 * @return array with length x
-//	 */
-//	private char[] createRoute(int numCities, String cities) {
-//		route = new char[numCities];
-//		for (int i = 0; i < cities.length(); i++) {
-//			route[i] = cities.charAt(i);
-//		}		
-//		return route;
-//	}
-//
-//	/**
-//	 * Calculate cost of route
-//	 * 
-//	 * @param route
-//	 * @return
-//	 */
-//	private int getCostOfRoute(char[] route) {
-//		int totalCost = 0;
-//		for (int i = 0; i < route.length - 1; i++) {
-//			char start = route[i];
-//			char next = route[i + 1];
-//			int startIndex = findGraphValue(start);
-//			int nextIndex = findGraphValue(next);
-//			int val = graph[startIndex][nextIndex];
-//			
-//			totalCost = totalCost + val;
-//		}
-//		
-//		int firstCity = findGraphValue(route[0]);
-//		int finalCity = findGraphValue(route[route.length - 1]);
-//		totalCost = totalCost + graph[finalCity][firstCity];
-//		return totalCost;
-//	}
-//
-//	/**
-//	 * Translate char into numerical value to find on graph
-//	 * 
-//	 * @param letter
-//	 *            - A/B/C/D
-//	 * @return int - 0/1/2/3, returns 9 if letter not found
-//	 */
-//	private int findGraphValue(char letter) {
-//		switch (letter) {
-//		case 'A':
-//			return A;
-//		case 'B':
-//			return B;
-//		case 'C':
-//			return C;
-//		case 'D':
-//			return D;
-//		default:
-//			return 9;
-//		}
-//	}
-//
-//	private String generateRandomRoute() {
-//		String route = "A";
-//		ArrayList<Character> cities = new ArrayList<Character>();
-//		cities.add('B');
-//		cities.add('C');
-//		cities.add('D');
-//
-//		int length = cities.size();
-//		for (int i = 0; i < length; i++) {
-//			int randomCity = new Random().nextInt(cities.size());
-//			route = route + cities.get(randomCity);
-//			cities.remove(randomCity);
-//		}
-//		return route;
-//	}
-//	
-//	private String generateTwoOptNeighbour(String route){
-//		Random generator = new Random(); 
-//		char[] charRoute = route.toCharArray();
-//		int i = generator.nextInt(route.length());
-//		int u = generator.nextInt(route.length());
-//		
-//		while(i == u){
-//			u = generator.nextInt(route.length());
-//		}
-//		
-//		char temp = charRoute[i];
-//		charRoute[i] = charRoute[u];
-//		charRoute[u] = temp;
-//		return new String(charRoute);
-//	}
-//	
-//	private Set<String> generateNeighbourhood(String route){	
-//		Set<String> neighbourhood = new HashSet<String>();
-//		neighbourhood.add(route);
-//		while(neighbourhood.size() < (((route.length()*(route.length() - 1))/2) + 1)){
-//			neighbourhood.add(generateTwoOptNeighbour(route));
-//		}
-//		return neighbourhood;
-//	}
-//	
-//	//Step 4
-//	private String neighbourhoodStep(Set<String> neighbourhood){
-//		int x=0;
-//		int leastCost=100000;
-//		String bestRoute = "";
-//		for(String route : neighbourhood){
-//			String randomRoute = route;
-//			x = getCostOfRoute((createRoute(4,randomRoute)));
-//			if(x<leastCost){
-//				leastCost = x;
-//				bestRoute = randomRoute;
-//			}
-//		}
-//		return bestRoute;
-//	}
-//	
-//	//Step 5
-//	private String randomLocalSearch(int setTime){
-//		long timer = System.currentTimeMillis() + setTime;
-//		int currentCost=0;
-//		int leastCost=100000;
-//		String bestRoute = "";
-//		while(System.currentTimeMillis() < timer){
-//			String randomRoute = neighbourhoodStep(generateNeighbourhood(generateRandomRoute()));
-//			currentCost = getCostOfRoute((createRoute(4,randomRoute)));
-//			if(currentCost<leastCost){
-//				leastCost = currentCost;
-//				bestRoute = randomRoute;
-//			}
-//		}
-//		return "The best route is " + bestRoute + " and it costs " + leastCost;
-//	}
-//	
-//	private String timedRoute(int setTime){
-//		long timer = System.currentTimeMillis() + setTime;
-//		int x=0;
-//		int leastCost=100000;
-//		String bestRoute = "";
-//		while(System.currentTimeMillis() < timer){
-//			String randomRoute = generateRandomRoute();
-//			x = getCostOfRoute((createRoute(4,randomRoute)));
-//			if(x<leastCost){
-//				leastCost = x;
-//				bestRoute = randomRoute;
-//			}
-//		}
-//		
-//		return "Cheapest route to take is " + bestRoute + " and it costs " + leastCost;
-//	}
+
 	
 	
 	/*CSV FILE CODE*/
@@ -358,8 +183,8 @@ public class Main {
 			
 		}
 		
-		for(int city1 = 0; city1<16; city1++) {
-			for(int city2=0; city2<16; city2++) {
+		for(int city1 = 0; city1<cityLocations.length; city1++) {
+			for(int city2=0; city2<cityLocations.length; city2++) {
 				double minusX = Math.pow((xValue[city2]-xValue[city1]), 2);
 				double minusY = Math.pow((yValue[city2]-yValue[city1]), 2);
 				//System.out.print(Math.sqrt(minusX + minusY) + "|");
