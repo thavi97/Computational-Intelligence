c312f5563cd9324b9824364b2ae7196e7b254e30
Thavi <thavit97@gmail.com>
2018-11-02 15:00:25 +0000

    FUCKLING FINSIHIONF UI?GTIITIT

--- Lab6/src/AIS.java
+++ Lab6/src/AIS.java
@@ -16,7 +16,7 @@ public class AIS {
 	private ArrayList<ArrayList<Integer>> clonePool;
 	private double bestCost;
 
-	public AIS(int populationNum) {
+	public AIS(int populationNum, int evaluations) {
 		
 		try {
 			loadFile(16);
@@ -25,13 +25,19 @@ public class AIS {
 		}
 		
 		initialisePopulation(populationNum);
+
 		normalisedFitness();
-		clone(populationNum, 3);
+		for(int i=0; i<evaluations; i++){
+			selection(clone(populationNum, 3), populationNum);
+			metadynamics(2, populationNum);
+		}
 		
+		ArrayList<Integer> bestRoute = bestRoute();
+		System.out.println("The best route is " + bestRoute + " and it costs " + getCostOfRouteCSV(bestRoute));
 	}
 
 	public static void main(String[] args) {	
-		new AIS(5);
+		new AIS(5, 5000);
 	}
 	
 	private ArrayList<ArrayList<Integer>> initialisePopulation(int populationSize){
@@ -66,31 +72,27 @@ public class AIS {
 			for(int u=0; u<populationNum*cloneSizeFactor; u++){
 				ArrayList<Integer> hyperMutation = hyperMutation(1, population.get(i), i);
 				clonePool.add(hyperMutation);
-				System.out.println(hyperMutation);
 			}	
 		}		
 		return clonePool;
 	}
 	
 	private ArrayList<Integer> hyperMutation(int rho, ArrayList<Integer> clonedRoute, int index){
-		ArrayList<Integer> clonedRouteDummy = new ArrayList<Integer>(clonedRoute);
+		ArrayList<Integer> newClone = new ArrayList<Integer>(clonedRoute);
 		double mutationRate = Math.exp((-1*rho)*normalisedFitness.get(index));
-		int blockLength = (int)(clonedRouteDummy.size()* mutationRate);
-		ArrayList<Integer> newClone = new ArrayList<Integer>();
+		int blockLength = (int)(newClone.size()* mutationRate);
 		ArrayList<Integer> blockClone = new ArrayList<Integer>();
 		
 		Random random = new Random();
-		int startIndex = random.nextInt(clonedRouteDummy.size());
-		for(int i=0; i<clonedRouteDummy.size(); i++){
-			newClone.add(-1);
-		}
+		int startIndex = random.nextInt(newClone.size());
+
 		int u1 = startIndex;
 		for(int i=startIndex; i<startIndex+blockLength; i++){
-			if(u1 == clonedRouteDummy.size()) {
+			if(u1 == newClone.size()) {
 				u1 = 0;
 			}
-			blockClone.add(clonedRouteDummy.get(u1));
-			clonedRouteDummy.set(u1, -1);
+			blockClone.add(newClone.get(u1));
+			newClone.set(u1, -1);
 			u1++;
 		}
 		
@@ -98,22 +100,90 @@ public class AIS {
 		
 		int u2 = startIndex;
 		int u3 = 0;
-		for(int i=startIndex; i<startIndex+blockLength; i++){
-			if(i==clonedRouteDummy.size()){
+		for(int i=0; i<blockLength; i++){
+			if(u2==newClone.size()){
 				u2=0;
 			}
-			clonedRouteDummy.set(u2, blockClone.get(u3));
+			newClone.set(u2, blockClone.get(u3));
 			u2++;
 			u3++;
 		}
 		
-		System.out.println("start index = " + startIndex);
-		System.out.println("block length = " + blockLength);
-		System.out.println("cloned route = " + clonedRoute);
-		System.out.println("block clone = " + blockClone);
-		System.out.println("new clone = " + clonedRouteDummy);
-		System.out.println("--------------------------------------");
-		return clonedRouteDummy;
+//		System.out.println("start index = " + startIndex);
+//		System.out.println("block length = " + blockLength);
+//		System.out.println("cloned route = " + clonedRoute);
+//		System.out.println("block clone = " + blockClone);
+//		System.out.println("new clone = " + newClone);
+//		System.out.println("--------------------------------------");
+		return newClone;
+	}
+	
+	
+	private ArrayList<ArrayList<Integer>> selection(ArrayList<ArrayList<Integer>> clonePool, int populationSize){
+		for(int i=0; i<clonePool.size(); i++){
+			population.add(clonePool.get(i));
+		}
+		
+		while(population.size() > populationSize){
+			ArrayList<Integer> thisRoute = null;
+			ArrayList<Integer> worstRoute = null;
+			double thisCost = 0;
+			double worstCost = 0;
+			for(int i=0; i<population.size(); i++){
+				thisRoute = population.get(i);
+				thisCost = getCostOfRouteCSV(thisRoute);
+				if(thisCost>worstCost){
+					worstCost = thisCost;
+					worstRoute = thisRoute;
+				}
+			}
+			population.remove(population.indexOf(worstRoute));
+		}
+		
+		normalisedFitness();
+		
+		return population;
+	}
+	
+	private ArrayList<ArrayList<Integer>> metadynamics(int d, int populationSize){
+		for(int u=0; u<d; u++){
+			ArrayList<Integer> thisRoute = null;
+			ArrayList<Integer> worstRoute = null;
+			double thisCost = 0;
+			double worstCost = 0;
+			for(int i=0; i<population.size(); i++){
+				thisRoute = population.get(i);
+				thisCost = getCostOfRouteCSV(thisRoute);
+				if(thisCost>worstCost){
+					worstCost = thisCost;
+					worstRoute = thisRoute;
+				}
+			}
+			population.remove(population.indexOf(worstRoute));
+		}
+		for(int i=0; i<d; i++){
+			population.add(generateRandomRouteCSV());
+		}
+		normalisedFitness();
+		
+		return population;
+	}
+	
+	private ArrayList<Integer> bestRoute(){
+		ArrayList<Integer> bestRoute = null;
+		ArrayList<Integer> thisRoute = null;
+		double thisCost = 0;
+		double bestCost1 = 10000;
+		for(int i=0; i<population.size(); i++){
+			thisRoute = population.get(i);
+			thisCost = getCostOfRouteCSV(thisRoute);
+			if(thisCost<bestCost1){
+				bestCost1 = thisCost;
+				bestRoute = thisRoute;
+
+			}
+		}
+		return bestRoute;
 	}
 	
 	/*-------------------------------------------------------------------*/
