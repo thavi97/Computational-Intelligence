d0870987d1593e0d5ce42614bbd920ef2db32075
Thavi <thavit97@gmail.com>
2018-11-02 13:32:46 +0000

    New clone (Reversed)

--- Lab6/src/AIS.java
+++ Lab6/src/AIS.java
@@ -76,7 +76,6 @@ public class AIS {
 		ArrayList<Integer> clonedRouteDummy = new ArrayList<Integer>(clonedRoute);
 		double mutationRate = Math.exp((-1*rho)*normalisedFitness.get(index));
 		int blockLength = (int)(clonedRouteDummy.size()* mutationRate);
-		int nonBlockLength = clonedRouteDummy.size() - blockLength;
 		ArrayList<Integer> newClone = new ArrayList<Integer>();
 		ArrayList<Integer> blockClone = new ArrayList<Integer>();
 		
@@ -85,24 +84,34 @@ public class AIS {
 		for(int i=0; i<clonedRouteDummy.size(); i++){
 			newClone.add(-1);
 		}
-		int u = startIndex;
+		int u1 = startIndex;
 		for(int i=startIndex; i<startIndex+blockLength; i++){
-			if(u == clonedRouteDummy.size()) {
-				u = 0;
+			if(u1 == clonedRouteDummy.size()) {
+				u1 = 0;
 			}
-			blockClone.add(clonedRouteDummy.get(u));
-			clonedRouteDummy.set(u, -1);
-			u++;
+			blockClone.add(clonedRouteDummy.get(u1));
+			clonedRouteDummy.set(u1, -1);
+			u1++;
 		}
 		
-		ArrayList<Integer> nonBlockClone = new ArrayList<Integer>(clonedRouteDummy);
-		nonBlockClone.removeAll(Collections.singleton(-1));
+		Collections.reverse(blockClone);
+		
+		int u2 = startIndex;
+		int u3 = 0;
+		for(int i=startIndex; i<startIndex+blockLength; i++){
+			if(i==clonedRouteDummy.size()){
+				u2=0;
+			}
+			clonedRouteDummy.set(u2, blockClone.get(u3));
+			u2++;
+			u3++;
+		}
 		
 		System.out.println("start index = " + startIndex);
 		System.out.println("block length = " + blockLength);
 		System.out.println("cloned route = " + clonedRoute);
 		System.out.println("block clone = " + blockClone);
-		System.out.println("non block clone = " + nonBlockClone);
+		System.out.println("new clone = " + clonedRouteDummy);
 		System.out.println("--------------------------------------");
 		return clonedRouteDummy;
 	}
